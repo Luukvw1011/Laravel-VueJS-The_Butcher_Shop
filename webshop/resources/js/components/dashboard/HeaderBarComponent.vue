bd397b28f547fe92fa5b937a5e430c92bd344e56
Luuk van Wijgerden <113586650+Luukvw1011@users.noreply.github.com>
2023-12-02 15:04:16 +0100

    webshop account logout: Added the logout functionality to the account page.

--- webshop/resources/js/components/dashboard/HeaderBarComponent.vue
+++ webshop/resources/js/components/dashboard/HeaderBarComponent.vue
@@ -1,24 +1,14 @@
 <template>
     <div class="bg-primary text-white d-flex align-items-center px-2 justify-content-between">
-        <h4 class="d-lg-block d-none">User: testUser1</h4>
         <icon @click="openSideBar" class="fs-1 d-lg-none" icon="fa-solid fa-bars" />
         <h2>Webshop</h2>
     </div>
 </template>
 
-<script>
-export default {
-    setup() {
-
-        function openSideBar () {
-            document.getElementById("sidebar").classList.remove("d-none");
-        }      
-
-        return {
-            openSideBar,
-        } 
-    }
-}
+<script setup>
+    function openSideBar () {
+        document.getElementById("sidebar").classList.remove("d-none");
+    }      
 </script>
 
 <style scoped>
