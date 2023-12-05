d504556707deca43c4aa0cc3dcc646cb13c2a763
Luuk van Wijgerden <113586650+Luukvw1011@users.noreply.github.com>
2023-12-05 19:07:03 +0100

    webshop user store: Added a user store for handling the logged in user data.

--- webshop/resources/js/components/dashboard/DashboardLayout.vue
+++ webshop/resources/js/components/dashboard/DashboardLayout.vue
@@ -7,16 +7,17 @@
         <div class="col-lg-10 col-12">
             <HeaderBarComponent></HeaderBarComponent>
             <div class="container mt-3">
-                <!-- Router view -->
                 <router-view></router-view> 
             </div>
         </div>
     </div>
 </template>
+
 <script setup>
     import HeaderBarComponent from './HeaderBarComponent.vue';
-    import SideBarComponent from './SideBarComponent.vue';
+    import SideBarComponent from './SideBarComponent.vue'; 
 </script>
+
 <style> 
     body {
         background-color: rgb(95, 95, 95);
